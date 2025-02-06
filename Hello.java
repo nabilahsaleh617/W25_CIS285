5d940a94637f717e201133bb7fc83332ae5a7ef1
nabilahsaleh617 <nabilahsaleh090@gmail.com>
2025-02-06 14:19:50 -0500

    Loop 12

--- Hello.java
+++ Hello.java
@@ -1,11 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
         for (int i = 1; i <= 12; i++) {
-=======
-        for (int i = 1; i <= 11; i++) {
->>>>>>> b6a14935432bf9cb6e85e064c663bed4527eec50
 
             System.out.println("Hello World..." + i);
         }
